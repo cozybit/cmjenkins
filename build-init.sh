5ce317e796cfe40de4b5812c619be780459f5a3c
Jason Mobarak <jam@cozybit.com>
2014-02-28 11:54:56 -0800

    Exclude new files from clean

--- build-init.sh
+++ build-init.sh
@@ -7,7 +7,10 @@ find . -maxdepth 1 -not \( \
   -name 'build*.sh' -or \
   -name '.repo' -or \
   -name 'jenkins_*' -or \
-  -name 'bin' \) \
-  -exec rm -rf {} \;
+  -name 'bin' -or \
+  -name '.git' -or \
+  -name 'local_manifest.xml' -or \
+  -name '.gitignore' \) \
+  -exec echo rm -rf {} \;
 
 repo init -u /cyanogenmod/cm/cozybit/android.git -b cm-10.2
